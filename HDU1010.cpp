f0b6ba318bab610bca63c612bb5ffedc98150a53
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-01-09 22:53:08 +0800

    Update HDU1010.cpp

--- HDU1010.cpp
+++ HDU1010.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include <algorithm>
-#include <queue>
 #include <iostream>
 using namespace std;
 struct Node {
@@ -9,36 +8,26 @@ struct Node {
 };
 int n,m,t;
 char map[10][10];
-bool visited[10][10];
+int visited[10][10];
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-queue<Node> q;
 
-bool bfs() {
-	while(!q.empty()) {
-		Node p=q.front();
-		q.pop();
-		for(int i=0; i<4; i++) {
-				int nx=p.x+dx[i];
-				int ny=p.y+dy[i];
-				int nt=p.time+1;
-				if( nx<0 || nx>=n || ny<0 || ny>=m || visited[nx][ny]==true || map[nx][ny]=='X') {
-					continue;
-				}
-				else if(map[nx][ny]=='D' && p.time==t) {
-					return true;
-				}
-				if(map[nx][ny]=='.')
-					visited[nx][ny]=true;
-				
-				Node next;
-				next.x=nx;
-				next.y=ny;
-				next.time=nt;
-				q.push(next);
+int dfs(Node p) {
+	for(int i=0; i<4; i++) {
+		int nx=p.x+dx[i];
+		int ny=p.y+dy[i];
+		int nt=p.time+1;
+		if( nx>=0 && nx<n && ny>=0 && ny<m && visited[nx][ny]==0 && map[nx][ny]=='.') {
+			Node next;
+			next.x=nx; next.y=ny; next.time=nt;
+			if(dfs(next)) return 1;
+			visited[nx][ny]=0;
+		}
+		if(map[nx][ny]=='D' && t==p.time) {
+			return 1;
 		}
 	}
-	return false;
+	return 0;
 }
 int main() {
 while(scanf("%d %d %d", &n, &m, &t)) {
@@ -52,15 +41,12 @@ while(scanf("%d %d %d", &n, &m, &t)) {
 				p.x=i;
 				p.y=j;
 				p.time=0;
-				visited[i][j]=true;
+				visited[i][j]=1;
 			}
 		}
 	}
-	while(!q.empty()) {
-		q.pop();
-	}
-	q.push(p);
-	bool ans=bfs();
+	
+	int ans=dfs(p);
 	if(ans) {
 		cout <<"YES"<< endl;
 	}
@@ -68,7 +54,7 @@ while(scanf("%d %d %d", &n, &m, &t)) {
 		cout <<"NO"<< endl;
 	}
 }
-} 
+}
 /*
 2 4 7
 S..D
