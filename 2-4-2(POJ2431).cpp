dccecd5196b517a9ce564f019eb58ad53234ad4d
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-01-12 13:29:04 +0800

    Update 2-4-2(POJ2431).cpp

--- 2-4-2(POJ2431).cpp
+++ 2-4-2(POJ2431).cpp
@@ -6,23 +6,30 @@
 using namespace std;
 const int MAXN=1e4+5;
 int N;
-int A[MAXN],B[MAXN];
+struct Node {
+	int a,b;
+}A[MAXN];
 int L, P;
 priority_queue<int> que;
+bool cmp(const Node &a1,const Node &a2)
+{
+	return a1.a>a2.a;
+}
+
 int main() {
 	cin >> N;
 	for(int i=0; i<N; i++) {
-		cin >> A[i] >> B[i];
+		cin >> A[i].a >> A[i].b;
 	}
 	cin >> L >> P;
 	
-	A[N]=0;
-	B[N]=0;
+	A[N].a=0;
+	A[N].b=0;
 	N++;
-
+	sort(A,A+N,cmp);
 	int ans=0, pos=0, tank =P;
 	for(int i=0; i<N; i++) {
-		int d = L-A[i]-pos;
+		int d = L-A[i].a-pos;
 		while( tank-d<0) {
 			if(que.empty()) {
 				cout <<"-1";
@@ -32,8 +39,8 @@ int main() {
 			ans++;
 		}
 		tank -= d;
-		pos = L-A[i];
-		que.push(B[i]);
+		pos = L-A[i].a;
+		que.push(A[i].b);
 	}
 	cout << ans;
 }
