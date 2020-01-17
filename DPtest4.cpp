705b07d189d52d373925e289aa440275ff063a5c
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-01-17 19:57:01 +0800

    Update DPtest4.cpp

--- DPtest4.cpp
+++ DPtest4.cpp
@@ -6,12 +6,15 @@ int a[10];
 bool dp[10][10];
 int N;
 int S;
+bool rec() {
+	
+}
+
 int main() {
 	cin >> N >> S;
 	for(int i=0; i<N; i++) {
 		cin >> a[i];
 	}
-	
 	for(int i=0; i<N; i++) {
 		for(int j=0; j<N; j++) {
 			if( j==0) dp[i][j] = true;
@@ -22,10 +25,11 @@ int main() {
 				dp[i][j] = dp[i-1][j];
 			}
 			else {
-				dp[i][j] = dp[i-1][j-a[i]];
+				dp[i][j] = (dp[i-1][j-a[i]] || dp[i-1][j]);
 			}
 		}
 	}
+	
 	if(dp[N-1][S])
 		cout << "true";
 	else 
