e2f2d2062ecfbdc7a7b6006c918af99f92f1cf07
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-01-19 13:35:18 +0800

    Create leetcode402.cpp

---
+++ leetcode402.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+    	string a;
+        for(int i=1; i<k; i++) {
+        	for(int j=0; j<num.length(); j++) {
+        		if( num[j]==i) {
+        			a[j] = true;
+				}
+			}
+			for(int j=0; j<)
+		}
+    }
+};
