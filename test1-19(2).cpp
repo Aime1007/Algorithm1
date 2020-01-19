83a697a16c68b5fdfdb412f8653ace9c1ef1120c
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-01-19 17:00:39 +0800

    Create test1-19(2).cpp

---
+++ test1-19(2).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+rec(int i, vector<int>& nums, vector<int>& item, vector<vector<int> >& result) {
+	
+}
+
+int main() {
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(3);
+	
+	vector<int> item;
+	vector<vector<int> > result;
+	
+	
+	for(int i=0; i<result.size(); i++) {
+		for(int j=0; j<result[i].size(); j++) {
+			cout << result[i][j];
+		}
+		cout <<endl;
+	}
+}
