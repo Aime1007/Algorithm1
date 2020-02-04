b8a6500273087baefb8fbcce672fc7dc1913464e
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-02-04 10:55:03 +0800

    Update test-FenZhi-chess.cpp

--- test-FenZhi-chess.cpp
+++ test-FenZhi-chess.cpp
@@ -1,26 +1,26 @@
-#include<stdio.h>
-int count=0,board[100][100];
-//覆盖以（tr，tc）为左上角坐标，宽为size的棋盘。残缺块或伪残缺块坐标为（dr，dc） 
-void cover(int tr,int tc,int size,int dr,int dc);
-int main()
-{
-    int x,y,k,size=1;
-    int i,j;
-    scanf("%d%d%d",&k,&x,&y);
-    for(i=1;i<=k;i++) size=size*2;//计算2^k 
-    cover(0,0,size,x,y);//对原始棋盘进行覆盖 
-    for(i=0;i<size;i++)//输出棋盘的覆盖结果 
-    {
-        for(j=0;j<size;j++)
-        {
-            printf("%-2d ",board[i][j]);
-        }
-        printf("\n");
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int count=1,board[100][100];
+
+void cover(int tr,int tc,int dr,int dc,int size);
+int main() {
+    int size;
+    cin>>size;
+    int dr, dc;
+    cin >> dr >> dc;
+    cover(0, 0, dr, dc, size);
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++)
+            cout << setw(5) << setiosflags(ios::left) << board[i][j];
+        cout << endl;
     }
     return 0;
 }
 
-void cover(int tr,int tc,int size,int dr,int dc)
+void cover(int tr,int tc,int dr,int dc,int size)
 {
     if(size==1) return ;
     int t=count++;
@@ -32,7 +32,7 @@ void cover(int tr,int tc,int size,int dr,int dc)
 		board[tr+s-1][tc+s-1]=t;
 		cover(tr, tc, tr+s-1, tc+s-1, s);
 	}
-	if(dr<tr+s && dc>tc+s) {
+	if(dr<tr+s && dc>=tc+s) {
 		cover(tr, tc+s, dr, dc, s);
 	}
 	else {
@@ -50,7 +50,7 @@ void cover(int tr,int tc,int size,int dr,int dc)
 		cover(tr+s, tc+s, dr, dc, s);
 	}
 	else {
-		board[tr=s][tc=s]=t;
+		board[tr+s][tc+s]=t;
 		cover(tr+s, tc+s, tr+s, tc+s, s);
 	}
 }
