7d8295ee8634e8d2ea75995d1bfa7846c8d54cb5
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-02-06 11:00:19 +0800

    1

--- test-DP-YingBi.cpp
+++ test-DP-YingBi.cpp
@@ -5,6 +5,8 @@ const int MAX_M=100;
 const int MAX_V=5;
 int dp[MAX_M];
 int value[MAX_V]={1,5,10,25,50};
+int Min_path[MAX_M]={0}; 
+
 void solve() {
 	for(int i=0; i<MAX_M; i++) {
 		dp[i]=INT_MAX;
@@ -12,15 +14,24 @@ void solve() {
 	dp[0]=0;
 	for(int i=0; i<MAX_V; i++) {
 		for(int j=value[i]; j<MAX_M; j++) {
-			dp[j]=min(dp[j], dp[j-value[i]]+1);
+			if(dp[j]>dp[j-value[i]]+1) {
+				Min_path[j] = value[i];
+				dp[j] = dp[j-value[i]]+1;
+			}
 		}
 	}
 }
-
+void print(int *Min_path, int s) {
+	while(s) {
+		cout << Min_path[s] <<" ";
+		s = s-Min_path[s];
+	}
+}
 int main() {
 	int s;
 	solve();
 	while(cin>>s) {
 		cout<< dp[s] <<endl;
+		print(Min_path, s);
 	}
 }
