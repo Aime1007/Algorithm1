666815f580e1b1d7d4775df11d1ff8a3c0719be3
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-02-08 22:00:34 +0800

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <cstdio>
+#define ll long long
 using namespace std;
-int a[105];
-int b[105];
-int gcd(int a, int b) {
-	if(b==0) return a;
+ll a[105];
+ll b[105];
+ll gcd(ll a, ll b) {
+	if( b==0) return a;
 	return gcd(b, a%b);
 }
 int main() {
 	int N;
 	cin >> N;
 	for(int i=0; i<N; i++) {
-		scanf("%d/%d", &a[i], &b[i]);
+		scanf("%lld/%lld", &a[i], &b[i]);
 	}
-	int sum1=a[0];//总分子 
-	int sum2=b[0];//总分母 
+	ll sum1=a[0];//总分子 
+	ll sum2=b[0];//总分母 
 	
 	for(int i=1; i<N; i++) {
-		int t=sum2*b[i]/gcd(sum2, b[i]);//t最小公倍数 
-		int fm=b[i]*(t/b[i]);
-		int fz=sum1*(t/sum2)+a[i]*(t/b[i]);
+		ll t=sum2*b[i]/gcd(sum2, b[i]);//t最小公倍数 
+		ll fm=b[i]*(t/b[i]);
+		ll fz=sum1*(t/sum2)+a[i]*(t/b[i]);
 		sum1=fz/gcd(fz, fm);
 		sum2=fm/gcd(fz, fm);
 	}
 
-	int ans=sum1/sum2;
+	ll ans=sum1/sum2;
 	if(ans!=0) {
 		cout << ans;
 		if(sum1%sum2!=0) {
 			cout<<" "<< sum1%sum2/gcd(sum1, sum2) <<"/"<< sum2/gcd(sum1, sum2);
 		}
+	}
+	else if(sum1==0) {
+		cout<<0;
 	}
 	else {
 		cout << sum1%sum2/gcd(sum1, sum2) <<"/"<< sum2/gcd(sum1, sum2);
