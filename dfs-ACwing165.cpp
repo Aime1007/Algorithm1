e1df89605983604ff19471e4f6b354a5f6e655a9
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-02-15 11:17:30 +0800

    Create dfs-ACwing165.cpp

---
+++ dfs-ACwing165.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, w;
+int c[20];
+int cab[20];
+int ans;
+void dfs(int now, int cnt) {
+	if(cnt >= ans) return ;
+	if(now == n+1) {
+		ans=min(ans, cnt);
+		return;
+	}
+	for(int i=1; i<=cnt; i++) {
+		if(cab[i]+c[now]<=w) {
+			cab[i] += c[now];
+			dfs(now+1, cnt);
+			cab[i] -= c[now];
+		}
+	}
+	cab[cnt+1] = c[now];
+	dfs(now+1, cnt+1);
+	cab[cnt+1] = 0;
+}
+int main() {
+	cin >> n >> w;
+	for(int i=1; i<=n; i++) {
+		cin >> c[i];
+	}
+	sort(c+1, c+n+1); reverse(c+1, c+n+1);
+	ans = n;
+	dfs(1, 0);
+	cout << ans <<endl;
+}
