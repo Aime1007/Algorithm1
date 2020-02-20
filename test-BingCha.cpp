212729781583562d223bf037ee9f8af37ce5af0b
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-02-20 12:42:11 +0800

    Create test-BingCha.cpp

---
+++ test-BingCha.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int parent[100000];
+int visited[100000];
+void init() {
+	for(int i=0; i<=99999; i++) {
+		parent[i]=i;
+	}
+}
+//int find1(int a) {
+//	return a == parent[a] ? a:find1(parent[a]);
+//}
+//int find2(int a) {
+//	while(a!=parent[a]) {
+//		a = parent[a];
+//	}
+//	return a;
+//}
+int find(int a) {
+	int root=a;
+	while(root!=parent[root]) {
+		root=parent[root];
+	}
+	while(a!=root) {
+		int t=parent[a];
+		parent[a]=root;
+		a=t;
+	}
+	return root;
+}
+void merge(int a,int b) {
+	a=find(a);
+	b=find(b);
+	if(a != b) parent[a]=parent[b];
+}
+int main(){
+	int n;
+	cin >>n;
+	init();
+	while(n--){
+		int k;cin >> k;
+		int kk=k;
+		int index,index2;
+		cin >>index;
+		k--;
+		while(k--){
+			scanf("%d",&index2);
+			merge(index,index2);
+		}
+		if(kk>1){
+			visited[index2]=1;
+		}
+	}
+	int m;
+	scanf("%d",&m);
+	int flag=0;
+	while(m--){
+		int index;
+		scanf("%d",&index);
+		if(find(index)==index&&visited[index]==0){
+//			if(flag==0){
+//				printf("%05d",index);
+//				flag=1;
+//			}
+//			else{
+				printf(" %05d",index);
+				flag=1;
+//			}
+			visited[index]=1;
+		}
+	}
+	if(flag==0){
+		printf("No one is handsome");
+	}
+	return 0;
+}
