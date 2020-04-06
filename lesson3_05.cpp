49bb12afd37bd9f804bdffe21c44fa4b20cd786e
Aime1007 <51432865+Aime1007@users.noreply.github.com>
2020-04-06 19:03:16 +0800

    Update lesson3_05.cpp

--- lesson3_05.cpp
+++ lesson3_05.cpp
@@ -2,73 +2,52 @@
 #include <algorithm>
 using namespace std;
 const int maxn = 1005;
-struct node {
-	int data;
-	bool v;
-}a[maxn],b[maxn];
-bool cmp(const node &a1, const node &a2) {
-	return a1.data>a2.data;
+int a[maxn];
+int b[maxn];
+bool cmp(const int &a, const int &b) {
+	return a>b;
 }
+
 int main() {
 	int n;
-	while(cin>>n && n) {
-		int ans=0;
-		for(int i=1; i<=n; i++) {
-			cin >> a[i].data;
-			a[i].v=false;
-		}
-		for(int i=1; i<=n; i++) {
-			cin >> b[i].data;
-			b[i].v=false;
-		}
-		sort(a+1,a+n+1,cmp);
-		sort(b+1,b+n+1,cmp);
-		int index=n;
-		for(int i=1; i<=n; i++) {
-			if(a[i].data<b[i].data) {
-				int flag=0;
-				for(int j=i-1; j>=1; j--) {
-					if(j<1) break;
-					if(a[j].v=false) {
-						if(a[j].data>b[i].data) {
-							a[j].v=true;
-							b[i].v=true;
-							flag=1;
-							break;
-						}
-					}
-				}
-				if(flag==0) {
-					int flag2=0;
-					for(int j=n; j>i; j--) {
-						if(a[j].v=false) {
-							a[j].v=true;
-							b[i].v=true;
-							flag2=1;
-							break;
-						}
-					}
-					if(flag2==0) ans++;
-				}
+    while (cin >> n && n) {
+        for (int i=1; i<=n; i++)
+            cin >> a[i];
+        for (int i=1; i<=n; i++)
+            cin >> b[i];
+        sort(a+1, a+n+1, cmp);
+        sort(b+1, b+n+1, cmp);
+        int sa=1,ea=n;
+        int sb=1,eb=n;
+        int ans=0;
+        while(sa<=ea && sb<=eb) {
+        	if(a[sa]>b[sb]) {
+        		ans++;
+        		sa++;
+        		sb++;
 			}
-			else if(a[i].data>=b[i].data){
-				int index=-1;
-				for(int j=i+1; j<=n; j++) {
-					if(j>n) break;
-					if(a[j].v=false) {
-						if(a[j].data>b[i].data) {
-							index=j;
-						}
-						else break;
-					}
+			else if(a[sa]<b[sb]) {
+				ans--;
+				ea--;
+				sb++;
+			}
+			else {
+				if(a[ea]>b[eb]) {
+					ans++;
+					ea--;
+					eb--;
 				}
-				if(index!=-1) {
-					a[index].v=true;
-					b[i].v=true;
+				else if(a[ea]<b[eb]) {
+					ans--;
+					ea--;
+					sb++;
 				}
 				else {
-					a[i].v=true;
-					b[i].v=true;
+					if(a[ea]<b[sb]) {
+						ans--;
+						ea--;
+						sb++;
+					}
 				}
 			}
 		}
